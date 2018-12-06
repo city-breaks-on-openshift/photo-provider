22246204561dbd1e7e448cc0cebe5ad0462d3d90
jamesread <contact@jwread.com>
2018-12-06 16:30:14 +0000

    A simple photo controller!

---
+++ Controllers/PhotosController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace photo_service.Controllers
+{
+    [Route("api/[controller]")]
+    public class PhotosController : Controller
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "Iceland", "Other" };
+        }
+
+        [HttpGet("{city}")]
+        public string Get(string city)
+        {
+			switch (city) {
+				case "Iceland": return "awesome";
+				default: return "erw..." + city + "?";
+			}
+        }
+    }
+}
